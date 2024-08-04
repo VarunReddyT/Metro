4fc83919cb7b480a4aa8587138a442020f99bd12
Varun-MSI <tvarun2014@gmail.com>
2024-08-04 21:31:54 +0530

    New

--- src/App.js
+++ src/App.js
@@ -8,14 +8,10 @@ import { TicketProvider } from "./Components/TicketContext";
 import Account from "./Components/Account";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
-// import Google from "./Components/Google";
-// import { GoogleOAuthProvider } from "@react-oauth/google";
 import Navbar from "./Components/Navbar";
-// import PaymentFormDisplay from "./Components/PaymentFormDisplay";
 
 function App() {
   return (
-    // <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_ID}>
     <TicketProvider>
     <Router>
       <Navbar/>
@@ -23,17 +19,22 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/tickets' element={<Tickets/>}/>
         <Route path="/payment" element={<PaymentForm />}/>
-        {/* <Route path="/payment" element={<PaymentFormDisplay />}/> */}
         <Route path="/bookedticket" element={<BookedTicket />}/>
         <Route path="/account" element={<Account />}/>
         <Route path="/login" element={<Login />}/>
         <Route path="/register" element={<Register />}/>
-        {/* <Route path="/google" element={<Google/>}/> */}
       </Routes>
     </Router>
     </TicketProvider>
-    // </GoogleOAuthProvider>
   );
 }
 
 export default App;
+
+// import Google from "./Components/Google";
+// import { GoogleOAuthProvider } from "@react-oauth/google";
+// import PaymentFormDisplay from "./Components/PaymentFormDisplay";
+// <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_ID}>
+// </GoogleOAuthProvider>
+{/* <Route path="/payment" element={<PaymentFormDisplay />}/> */}
+{/* <Route path="/google" element={<Google/>}/> */}
